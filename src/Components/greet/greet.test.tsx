30bb6c6cea38417170d7ba83b0e549289f7af845
Pankaj Changra <pankaj.changra@simsaw.com>
2023-05-01 20:33:34 +0530

    greet file first test case

---
+++ src/Components/greet/greet.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from "./greet"
+
+test("Greet Render Correctly", () => {
+    render(<Greet />)
+
+    // it will check proper upper/lower case of the text
+    const textEle1 = screen.getByText("Hello"); 
+
+    // this is failed case because proper upper/lower case doesn't match of the text
+    // const textEle1 = screen.getByText("hello"); 
+    
+    // to avoid proper upper/lower case of the text wrap word with slashes and end up i
+    const textEle2 = screen.getByText(/hello/i); 
+
+    expect(textEle1).toBeInTheDocument();
+    expect(textEle2).toBeInTheDocument();
+})
